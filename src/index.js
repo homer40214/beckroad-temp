732700c7653f9605f753ef23ba2af13b72481d36
Homer Lin <guan40214@gmail.com>
2023-04-29 10:14:57 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';//常見的root
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  //可以去除
+);
+
+
